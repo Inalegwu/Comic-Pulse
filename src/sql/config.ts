3faa091e94ef04610d92d3416bf29035ecdcd7ba
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2024-12-25 00:46:18 +0100

    using Deno.env for environment variables

--- src/sql/config.ts
+++ src/sql/config.ts
@@ -1,7 +1,6 @@
 import { Config, Redacted } from "effect";
-import { Env } from "../env.ts";
 
 export const DatabaseConfig = Config.succeed({
-  DATABASE_URL: Env.DATABASE_URL,
-  DATABASE_AUTH_TOKEN: Redacted.make(Env.DATABASE_AUTH_TOKEN),
+  DATABASE_URL: Deno.env.get("DATABASE_URL"),
+  DATABASE_AUTH_TOKEN: Redacted.make(Deno.env.get("DATABASE_AUTH_TOKEN")),
 });
