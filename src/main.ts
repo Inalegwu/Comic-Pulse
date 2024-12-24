27eb1ae25d6cd521c7570684ee0e4fcb3b737d99
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2024-12-24 12:59:12 +0100

    [chore] revert, it works

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,13 @@ import { Effect } from "effect";
 import { checkForComics } from "./handlers/check-for-comics.ts";
 import { notifyRelease } from "./handlers/notify-release.ts";
 
-// Deno.cron(
-//   "Check for new comics",
-//   "0 */2 * * 4",
-//   () =>
-//     Effect.runPromise(
-//       checkForComics,
-//     ),
-// );
 Deno.cron(
   "Check for new comics",
-  "*/1 * * * *",
-  () => Effect.runPromise(checkForComics),
+  "0 */2 * * 4",
+  () =>
+    Effect.runPromise(
+      checkForComics,
+    ),
 );
 
 Deno.cron(
