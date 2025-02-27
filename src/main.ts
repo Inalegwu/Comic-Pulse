a6aa2922ef579ab2c148b45ac89dded668e23031
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2025-02-27 23:44:52 +0100

    [fix] run cron only once a day to reduce usage

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { checkForComics } from "./handlers/check-for-comics.ts";
 import { checkForConnectMagazine } from "./handlers/fetch-connect-magazine.ts";
 import { informBroker } from "./handlers/inform-broker.ts";
 
-Deno.cron("Check for new comics", "* */2 * * 5", () =>
+Deno.cron("Check for new comics", "* 10 * * 5", () =>
 	Effect.runPromise(checkForComics),
 );
 
-Deno.cron("Inform Broker", "* */4 * * 4", () =>
+Deno.cron("Inform Broker", "* 10 * * 4", () =>
 	Effect.runPromise(informBroker),
 );
 
-Deno.cron("Fetch Connect Magazine", "0 0 */2 * *", () =>
+Deno.cron("Fetch Connect Magazine", "0 10 */2 * *", () =>
 	Effect.runPromise(checkForConnectMagazine),
 );
