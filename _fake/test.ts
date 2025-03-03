2419e61e5a93202e4aceea1827530c3372b0ede9
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2025-03-03 11:08:08 +0100

    making some things work in the test runner

--- _fake/test.ts
+++
@@ -1,3 +0,0 @@
-import { Effect } from "effect";
-
-export default Effect.gen(function* () {});
--- test-runner.ts
+++ test-runner.ts
@@ -1,40 +1,68 @@
 import { Console, Effect } from "effect";
-import { glob as globLoader } from "glob";
+import * as esbuild from "esbuild";
+import { glob } from "glob";
 import * as fs from "node:fs";
-import { Module } from "node:module";
+import Module from "node:module";
 import path from "node:path";
 import vm from "node:vm";
 
+type Ctx = {
+	module: Record<string, unknown>;
+};
+
+class VirtualMachine extends Effect.Service<VirtualMachine>()("@pulse/vm", {
+	// deno-lint-ignore require-yield
+	effect: Effect.gen(function* () {
+		const execute = Effect.fn(function* (filePath: string) {
+			const code = yield* Effect.try(() =>
+				fs.readFileSync(filePath, "utf8").toString(),
+			);
+
+			const context = vm.createContext({
+				// ...globalThis,
+				require: Module.createRequire(path.resolve(filePath)),
+				module: {},
+			});
+
+			yield* Effect.sync(() => vm.runInContext(code, context)).pipe(
+				Effect.orDie,
+			);
+
+			return context as Ctx;
+		});
+
+		return { execute } as const;
+	}),
+}) {}
+
 const testRunner = Effect.gen(function* () {
-	const g = yield* Effect.tryPromise(
+	const vm = yield* VirtualMachine;
+
+	yield* Effect.tryPromise(
 		async () =>
-			// change this
-			await globLoader("_fake/**/*.ts", {
+			await esbuild.build({
+				// entryPoints: ["src/**/*.ts"],
+				entryPoints: ["_fake/fake.test.ts"],
+				outdir: ".temp/build",
+				format: "cjs",
+			}),
+	);
+
+	// .temp/build/tests/**/*.js
+	const files = yield* Effect.tryPromise(
+		async () =>
+			await glob(".temp/build/*.js", {
 				ignore: "node_modules/**",
 			}),
 	);
 
-	yield* Effect.logInfo(g);
-
-	yield* Effect.forEach(g, (filePath) =>
-		Effect.gen(function* () {
-			const context = vm.createContext({
-				...globalThis,
-			});
-
-			context.require = Module.createRequire(path.resolve(filePath));
-			context.module = { exports: {} };
-			context.exports = context.module.exports;
-
-			const code = yield* Effect.try(() =>
-				fs.readFileSync(filePath).toString(),
-			);
-
-			yield* Effect.sync(() => vm.runInContext(code, context));
-
-			yield* Effect.logInfo(context);
-		}),
-	);
-}).pipe(Effect.catchAll(Console.log));
+	yield* Effect.forEach(files, vm.execute).pipe(Effect.tap(Effect.logInfo));
+}).pipe(
+	Effect.catchAll(Console.log),
+	Effect.annotateLogs({
+		module: "runner",
+	}),
+	Effect.provide(VirtualMachine.Default),
+);
 
 Effect.runPromise(testRunner);
